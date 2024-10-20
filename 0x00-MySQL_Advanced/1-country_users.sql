46d0e278ec4a746ca9c091d78bf8f1c2e4bc3dee
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-10-20 05:36:43 +0100

    Made them executable

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- An SQL script that creates a table of users with unique contents
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
